fb83022889cddb4d6a4dc3083e3a4f7d98e6fb7d
mazeeqe <mazeeqe@protonmail.com>
2025-08-12 22:02:10 -0300

    Modified MCConsumerAlg.cpp

--- example/src/components/MCConsumerAlg.cpp
+++ example/src/components/MCConsumerAlg.cpp
@@ -4,6 +4,7 @@ DECLARE_COMPONENT(MCConsumerAlg)
 
 MCConsumerAlg::MCConsumerAlg(const std::string& name, ISvcLocator* svcLoc)
     : Gaudi::Algorithm(name, svcLoc) {
+  declareProperty("RecoParticleColl", m_recoParticleCollHandle, "RecoParticle collection");
 }
 
 MCConsumerAlg::~MCConsumerAlg() = default;
@@ -16,9 +17,16 @@ StatusCode MCConsumerAlg::initialize() {
 }
 
 StatusCode MCConsumerAlg::execute(const EventContext&) const {
+  const auto* recoColl = m_recoParticleCollHandle.get();
+  for (const auto& reconstructedParticle : *recoColl) {
+      float energy = reconstructedParticle.getEnergy();
+      int particleID = reconstructedParticle.getType();
+      // TODO: your code here, e.g., store in tree or print
+  }
+
   return StatusCode::SUCCESS;
 }
 
 StatusCode MCConsumerAlg::finalize() {
-  return StatusCode::SUCCESS;
+  return Gaudi::Algorithm::finalize();
 }
