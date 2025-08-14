8b30a027c1b9259b5e91efe457eccb0d7dc17884
mazeeqe <mazeeqe@protonmail.com>
2025-08-14 13:15:05 -0300

    Added PandoraPFOs to the consumerAlg

--- example/options/run_example.py
+++ example/options/run_example.py
@@ -4,14 +4,13 @@ from Configurables import PodioInput, MCProducerAlg, MCConsumerAlg
 from Configurables import k4DataSvc
 
 evtSvc = k4DataSvc('EventDataSvc')
-evtSvc.inputs = ["/afs/desy.de/user/b/bortolet/code/input_files/Dirac-Dst-E250-e2e2h_inv.eL.pR_bg-00002.root"]
+evtSvc.inputs = ["/afs/desy.de/user/b/bortolet/code/tomaz-example/input_files/Dirac-Dst-E250-e2e2h_inv.eL.pR_bg-00002.root"]
 
 # Input: PODIO .root file with MCParticles
 podioinput = PodioInput("InputReader")
 
 collections = ["PandoraPFOs", "PrimaryVertex", "PandoraClusters",
-                "MarlinTrkTracks", "EventHeader", "MCParticlesSkimmed",
-                "ReconstructedParticle"]
+                "MarlinTrkTracks", "EventHeader", "MCParticlesSkimmed"]
 
 podioinput.collections = collections
 
@@ -22,6 +21,7 @@ producer.OutputLevel = INFO
 
 # create a MCConsumer instance
 consumer = MCConsumerAlg("MCConsumer")
+consumer.RecoParticleColl = "PandoraPFOs"
 
 ApplicationMgr(
     # provide list and order of algorithms
