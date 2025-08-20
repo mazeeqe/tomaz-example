d4317f4b0a68ec8ba770600d802075ae7bc1d1ab
mazeeqe <mazeeqe@protonmail.com>
2025-08-20 13:33:43 -0300

    Added the histogram creation the MCConsumerAlg.cpp

--- example/src/components/MCConsumerAlg.cpp
+++ example/src/components/MCConsumerAlg.cpp
@@ -1,5 +1,10 @@
 #include "MCConsumerAlg.h"
 
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "TLorentzVector.h"
+
 DECLARE_COMPONENT(MCConsumerAlg)
 
 MCConsumerAlg::MCConsumerAlg(const std::string& name, ISvcLocator* svcLoc)
@@ -12,6 +17,11 @@ MCConsumerAlg::~MCConsumerAlg() = default;
 StatusCode MCConsumerAlg::initialize() {
   StatusCode sc = Gaudi::Algorithm::initialize();
   if (!sc.isSuccess()) return sc;
+
+  // Create ROOT file and histogram
+  m_rootFile = new TFile("hist.root", "RECREATE");
+  m_h_mumu = new TH1F("h_mumu", "Invariant Mass of #mu#mu; M_{#mu#mu} [GeV]; Events", 
+                      100, 0, 200);
 
   return StatusCode::SUCCESS;
 }
@@ -65,21 +75,32 @@ StatusCode MCConsumerAlg::execute(const EventContext&) const {
   auto& mu1 = muons[0];
   auto& mu2 = muons[1];
 
-  // Combined 4-vector
-  float E  = mu1.E  + mu2.E;
-  float px = mu1.px + mu2.px;
-  float py = mu1.py + mu2.py;
-  float pz = mu1.pz + mu2.pz;
+  // Calculate the Invariant Mass
+  TLorentzVector p4_1, p4_2;
+  p4_1.SetPxPyPzE(mu1.px, mu1.py, mu1.pz, mu1.E);
+  p4_2.SetPxPyPzE(mu2.px, mu2.py, mu2.pz, mu2.E);
 
-  float mass2 = E*E - (px*px + py*py + pz*pz);
-  float mass = (mass2 > 0) ? std::sqrt(mass2) : 0.0;
+  double invMass = (p4_1 + p4_2).M();
+
+  // Fill histogram
+  if (m_h_mumu) m_h_mumu->Fill(invMass);
+
 
   info() << "Invariant mass of two highest-pt muons = "
-         << mass << " GeV" << endmsg;
+         << invMass << " GeV" << endmsg;
 
   return StatusCode::SUCCESS;
 }
 
 StatusCode MCConsumerAlg::finalize() {
+  // Write to Root file
+    if (m_rootFile) {
+    m_rootFile->cd();
+    if (m_h_mumu) m_h_mumu->Write();
+    m_rootFile->Close();
+    delete m_rootFile;
+    m_rootFile = nullptr;
+  }
+
   return Gaudi::Algorithm::finalize();
 }
