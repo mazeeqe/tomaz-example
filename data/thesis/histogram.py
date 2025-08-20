3537e58d1f1688d7fd11bad9f5a76ea47b92fbdb
mazeeqe <mazeeqe@protonmail.com>
2025-08-20 13:19:38 -0300

    Modified the consumer to calculate the invariant mass of two muons and added a python file to plot the data

---
+++ data/thesis/histogram.py
@@ -0,0 +1,28 @@
+import uproot
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Open the ROOT file
+file = uproot.open("hist.root")
+
+# Access the histogram
+h_mumu = file["h_mumu"]
+
+# Get histogram values and bin edges
+values = h_mumu.values()
+edges = h_mumu.axes[0].edges()
+
+# Compute bin centers for plotting
+centers = 0.5 * (edges[:-1] + edges[1:])
+
+# Plot
+plt.figure(figsize=(8,6))
+plt.bar(centers, values, width=np.diff(edges), align="center",
+        edgecolor="black", color="skyblue")
+
+plt.xlabel(r"$M_{\mu\mu}$ [GeV]")
+plt.ylabel("Events")
+plt.title("Invariant Mass of Two Muons")
+plt.grid(alpha=0.3)
+
+plt.save("muon_histogram")
--- example/src/components/MCConsumerAlg.cpp
+++ example/src/components/MCConsumerAlg.cpp
@@ -19,36 +19,63 @@ StatusCode MCConsumerAlg::initialize() {
 StatusCode MCConsumerAlg::execute(const EventContext&) const {
   const auto* recoColl = m_recoParticleCollHandle.get();
 
-  // Total 4-momentum
-  float totalE = 0.0;
-  float totalPx = 0.0, totalPy = 0.0, totalPz = 0.0;
+  struct ParticleData {
+      float E;
+      float px, py, pz;
+      float pt2;
+  };
 
+  std::vector<ParticleData> muons;
+
+  // Select muons
   for (const auto& reconstructedParticle : *recoColl) {
-      int particleID = reconstructedParticle.getPDG();
-      if (particleID == 22) { // select gluons
-          float energy = reconstructedParticle.getEnergy();
-          auto momentum = reconstructedParticle.getMomentum();
+      int pdg = reconstructedParticle.getPDG();
+      if (std::abs(pdg) == 13) {
+          auto p = reconstructedParticle.getMomentum();
+          float E  = reconstructedParticle.getEnergy();
+          float px = p.x;
+          float py = p.y;
+          float pz = p.z;
+          float pt2 = px*px + py*py;
 
-          totalE  += energy;
-          totalPx += momentum.x;
-          totalPy += momentum.y;
-          totalPz += momentum.z;
+          muons.push_back({E, px, py, pz, pt2});
 
-          info() << "Selected PDG=21: E=" << energy
-                 << " px=" << momentum.x
-                 << " py=" << momentum.y
-                 << " pz=" << momentum.z
-                 << endmsg;
+          info() << "Muon candidate PDG=" << pdg
+                 << " E=" << E
+                 << " px=" << px
+                 << " py=" << py
+                 << " pz=" << pz
+                 << " pt2=" << pt2 << endmsg;
       }
   }
 
-  // Compute invariant mass if we found any gluons
-  if (totalE > 0) {
-      float mass2 = totalE*totalE - (totalPx*totalPx + totalPy*totalPy + totalPz*totalPz);
-      float mass = (mass2 > 0) ? std::sqrt(mass2) : 0.0;
-
-      info() << "Invariant mass of PDG=21 system: " << mass << " GeV" << endmsg;
+  // Need at least two muons
+  if (muons.size() < 2) {
+      info() << "Less than two muons found in event." << endmsg;
+      return StatusCode::SUCCESS;
   }
+
+  // Sort descending by pt2
+  std::sort(muons.begin(), muons.end(),
+            [](const ParticleData& a, const ParticleData& b) {
+                return a.pt2 > b.pt2;
+            });
+
+  // Take top two
+  auto& mu1 = muons[0];
+  auto& mu2 = muons[1];
+
+  // Combined 4-vector
+  float E  = mu1.E  + mu2.E;
+  float px = mu1.px + mu2.px;
+  float py = mu1.py + mu2.py;
+  float pz = mu1.pz + mu2.pz;
+
+  float mass2 = E*E - (px*px + py*py + pz*pz);
+  float mass = (mass2 > 0) ? std::sqrt(mass2) : 0.0;
+
+  info() << "Invariant mass of two highest-pt muons = "
+         << mass << " GeV" << endmsg;
 
   return StatusCode::SUCCESS;
 }
