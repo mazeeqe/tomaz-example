57bd31a75a01683900024c96b85fe7e5333b7282
mazeeqe <mazeeqe@protonmail.com>
2025-08-19 23:15:16 -0300

    Added the invariant mass calculation

--- example/src/components/MCConsumerAlg.cpp
+++ example/src/components/MCConsumerAlg.cpp
@@ -19,13 +19,30 @@ StatusCode MCConsumerAlg::initialize() {
 StatusCode MCConsumerAlg::execute(const EventContext&) const {
   const auto* recoColl = m_recoParticleCollHandle.get();
   for (const auto& reconstructedParticle : *recoColl) {
-      float energy = reconstructedParticle.getEnergy();
       int particleID = reconstructedParticle.getPDG();
-      
-      
-      // Print out the values
-      info() << "ReconstructedParticle: Energy = " << energy
-             << " GeV, PDG ID = " << particleID << endmsg;
+      if (particleID == 22) { // select gluons
+          float energy = reconstructedParticle.getEnergy();
+          auto momentum = reconstructedParticle.getMomentum();
+
+          totalE  += energy;
+          totalPx += momentum.x;
+          totalPy += momentum.y;
+          totalPz += momentum.z;
+
+          info() << "Selected PDG=21: E=" << energy
+                 << " px=" << momentum.x
+                 << " py=" << momentum.y
+                 << " pz=" << momentum.z
+                 << endmsg;
+      }
+  }
+
+  // Compute invariant mass if we found any gluons
+  if (totalE > 0) {
+      float mass2 = totalE*totalE - (totalPx*totalPx + totalPy*totalPy + totalPz*totalPz);
+      float mass = (mass2 > 0) ? std::sqrt(mass2) : 0.0;
+
+      info() << "Invariant mass of PDG=21 system: " << mass << " GeV" << endmsg;
   }
 
   return StatusCode::SUCCESS;
