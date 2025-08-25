7cb1f6e7d0d47c392535dc2e5a0732cfd871018a
mazeeqe <mazeeqe@protonmail.com>
2025-08-25 12:36:22 -0300

    Changed the histogram.py to acomodate the ROOT tree

--- data/thesis/histogram.py
+++ data/thesis/histogram.py
@@ -4,46 +4,26 @@ import numpy as np
 
 # Constants
 z_mass = 91.1880
+h_mass = 125.20
 
 # Open the ROOT file
 file = uproot.open("hist.root")
+tree = file["events"]
 
-def file_reader(column: str):
-        data = file[column]
 
-        # Get histogram values and bin edges
-        values = data.values()
-        edges = data.axes[0].edges()
+# Convert branches to numpy arrays
+invMass = tree["invMass"].array(library="np")
+recoilMass = tree["recoilMass"].array(library="np")
 
-        # Compute bin centers for plotting
-        centers = 0.5 * (edges[:-1] + edges[1:])
-
-        return centers, values
-
-# Access the histogram
-h_mumu = file["h_mumu"]
-h_recoil = file["h_recoil"]
-
-# Get histogram values and bin edges
-values = h_mumu.values()
-edges = h_mumu.axes[0].edges()
-
-# Compute bin centers for plotting
-centers = 0.5 * (edges[:-1] + edges[1:])
-
-mumu_centers, mumu_values = file_reader("h_mumu")
-recoil_centers, recoil_values = file_reader("h_recoil")
 
 # Plot
 plt.figure(figsize=(8,6))
 
 #Invariant mass
-plt.bar(mumu_centers, mumu_values, width=np.diff(edges), align="center",
-        edgecolor="black", color="skyblue")
+plt.hist(invMass, bins=100, range=(0,200), histtype='step', color='blue')
 
 #Recoil mass
-plt.bar(recoil_centers, recoil_values, width=np.diff(edges), align="center",
-        edgecolor="black", color="green")
+plt.hist(recoilMass, bins=100, range=(0,200), histtype='step', color='green')
 
 # Vertical line for Z mass
 plt.axvline(
@@ -54,9 +34,18 @@ plt.axvline(
     label=f'Constant = {z_mass}'
 )
 
+# Vertical line for Z mass
+plt.axvline(
+    h_mass,
+    color='yellow',            # line colour
+    linestyle='--',        # dashed line (solid, dashdot, etc.)
+    linewidth=2,
+    label=f'Constant = {h_mass}'
+)
+
 plt.xlabel(r"$M_{\mu\mu}$ [GeV]")
 plt.ylabel("Events")
 plt.title("Invariant Mass and Recoil Mass of Two Muons")
 plt.grid(alpha=0.3)
 
-plt.savefig("muon_histogram")
+plt.savefig("muon_histogram.png")
